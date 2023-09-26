2fbfa3cf312f64e28a04c2c793c3e3448bf88758
학원 <test@test.com>
2023-09-26 15:41:43 +0900

    230926예제

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -1,18 +1,31 @@
 package com.example.lesson03.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.lesson03.domain.Review;
+import com.example.lesson03.mapper.ReviewMapper;
 
 @Service
 public class ReviewBO {
 
+	@Autowired
+	private ReviewMapper reviewMapper;
 	//input:x(parameter가 없으니)
-	
+		//id
 	//output: review(단건)
 	
-	public Review getReview() {
-		
-		return ;
+	public Review getReview(int id) {
+		return reviewMapper.SelectReview(id);
+	}
+	//input: review(단건)
+	//output: int(성공된 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	//addReviewAsField(4,"콤비네이션R","김바다", 5.0,"역시 맛있다!!!");
+	public int addReviewAsField(int storeId, String menu, String userName, Double point, String review  ) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
 	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -7,6 +8,15 @@ import com.example.lesson03.domain.Review;
 @Repository
 public interface ReviewMapper {
 
-	public Review SelectReview(); 
-	
+	public Review SelectReview(int id); 
+
+	// return int(mybatis가 성공된 행의 개수를 리턴해준다)
+	public int insertReview(Review review);
+	public int insertReviewAsField(
+			//파라미터가 2개이상이면 map으로 만든다(@param) => map으로 만들어 주는 어노테이션
+			@Param("storeId")int storeId, 
+			@Param("menu")String menu, 
+			@Param("userName")String userName, 
+			@Param("point")Double point, 
+			@Param("review")String review);
 }
